6cf86cf048b3592edcae4bdca9adf013562da637
Rachit Malik <rachitmalik1947@gmail.com>
2020-03-28 13:50:34 +0530

    Added navbar:

---
+++ src/components/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+
+   
+        <el-header>
+          <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
+            <el-menu-item index="1">Help daily wage workers</el-menu-item>
+            <el-menu-item index="2">Find your local charity</el-menu-item>
+          </el-menu>
+        </el-header>
+
+  
+</template>
+
+<script>
+export default {
+     name: 'NavBar',
+     data(){
+         return {
+             activeIndex:'0'
+         }
+     },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+}
+}
+</script>
+
+<style  scoped>
+
+.el-menu {
+  border-right: 100 !important; 
+}
+  
+</style>
