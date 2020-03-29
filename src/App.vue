0c15cf07152d5380d05a453398ae23bc866776f9
Adel <adel-s@outlook.com>
2020-03-28 21:15:26 -0700

    Setup proper structure for app

--- src/App.vue
+++ src/App.vue
@@ -4,31 +4,18 @@
     <NavBar/>
     </el-header>
     <el-main>
-     <h1>COVID-19 charity match - we connect you to charities you can donate to provide relief for COVID-19 </h1>
-     <h2> Do your part to help daily wage workers in India! </h2>
-     <p> Nearly 90% of India’s workforce belongs to the informal sector. That means, about 450 million Indians depend on daily wages to run their household.(1)
-
-With the recent lockdowns in light of the Covid-19 pandemic, almost all of us have quickly adapted to working from home, while a large part of our community has been going to bed without knowing if they will be able to afford a meal for their family the next day. Workers at construction sites, shops, restaurants, delivery staff and local transport systems, the daily wager community has seen their only source of income come to a screeching halt, leaving them with no means to earn themselves a meal during these days of lockdown. Hunger and desperation prevails, and we can do something to help our fellow citizens.
-</p>
-<p> Find a list of charities dedicated to helping daily wage workers in the Indian subcontinent below and simply click to donate!  </p>
-<p> If you are not in India and want to donate to other causes </p> 
-<el-button type="primary" round> Click here! </el-button>
-<CharityList/>
+      <router-view></router-view>
     </el-main>
   </el-container>
-
-  
 </template>
 
 <script>
 
 import NavBar from './components/NavBar'
-import CharityList from './components/CharityList'
 export default {
   name: 'app',
   components: {
     NavBar,
-    CharityList
   }
 }
 </script>
---
+++ src/views/Local.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-container class="local">
+      <el-row :gutter="20">
+        <el-col :span="12" :offset="6">
+            <div class="grid-content bg-purple">
+                Hey ther delilah
+            </div>
+        </el-col>
+    </el-row>
+  </el-container>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'local',
+  components: {
+    
+  }
+}
+</script>
