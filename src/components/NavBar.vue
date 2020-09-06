27289e272058e897bac528b4f29879f0cbf2dc17
Rachit Malik <rachitmalik1947@gmail.com>
2020-09-06 18:56:26 +0530

    removed yarn.lock

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,6 +2,7 @@
   <el-menu :default-active="activeIndex" @select="handleSelect" class="el-menu-demo" mode="horizontal">
     <el-menu-item index="1">Help daily wage workers</el-menu-item>
     <el-menu-item index="2">Find a charity in the US</el-menu-item>
+    <el-menu-item index="3">My saved charities</el-menu-item>
   </el-menu>
 </template>
 
